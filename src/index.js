2ec51f905f8c01b7b5da904dab8755811751ca8e
Pavel Dolgov <70911716+mamont79@users.noreply.github.com>
2021-03-16 01:01:27 +0300

    add: partly solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let i = 0;
+    let n = 0;
+    let m = 0;
+    let k = 0;
+    while (i < str.length) {
+        if (str[i] == '(') {
+            n = n + 1;
+        } else if (str[i] == ')') {
+            n = n - 1;
+            if (n < 0) {
+                return false;
+            }
+        }
+
+        i++;
+    }
+
+    while (i < str.length) {
+        if (str[i] == '[') {
+            m = m + 1;
+        } else if (str[i] == ']') {
+            m = m - 1;
+            if (m < 0) {
+                return false;
+            }
+        }
+
+        i++;
+    }
+
+    while (i < str.length) {
+        if (str[i] == '|') {
+            k = k + 1;
+        } else if (str[i] == '}') {
+            k = k - 1;
+            if (k < 0) {
+                return false;
+            }
+        }
+
+        i++;
+    }
+
+    if (n == 0 && m == 0 && k == 0) {
+        return true;
+    } else if (n != 0 || m != 0 || k != 0) {
+        return false;
+    }
 }
