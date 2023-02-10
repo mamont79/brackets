e2757ae7f67b326acda9293bbc7258804b0013fb
Pavel Dalhou <70911716+mamont79@users.noreply.github.com>
2023-02-10 20:28:58 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,50 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-    let i = 0;
-    let n = 0;
-    let m = 0;
-    let k = 0;
-    while (i < str.length) {
-        if (str[i] == '(') {
-            n = n + 1;
-        } else if (str[i] == ')') {
-            n = n - 1;
-            if (n < 0) {
-                return false;
-            }
+    let arr = [...str];
+
+    if (str.length % 2 !== 0) {
+        return false
+    } else {
+
+        for (let i = 0; i < arr.length; i++) {
+            bracketsConfig.forEach((symbol) => {
+                if (arr[i] === symbol[0] && arr[i + 1] === symbol[1]) {
+                    arr.splice(i, 2);
+                    i = -1;
+                }
+            })
         }
+        return arr.length === 0;
 
-        i++;
-    }
-
-    while (i < str.length) {
-        if (str[i] == '[') {
-            m = m + 1;
-        } else if (str[i] == ']') {
-            m = m - 1;
-            if (m < 0) {
-                return false;
-            }
-        }
-
-        i++;
-    }
-
-    while (i < str.length) {
-        if (str[i] == '|') {
-            k = k + 1;
-        } else if (str[i] == '}') {
-            k = k - 1;
-            if (k < 0) {
-                return false;
-            }
-        }
-
-        i++;
-    }
-
-    if (n == 0 && m == 0 && k == 0) {
-        return true;
-    } else if (n != 0 || m != 0 || k != 0) {
-        return false;
     }
 }
